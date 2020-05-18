90ff153721d487d2fecc5fae0ab59c3866cb27ab
PavneetG <pavneetgill2@live.com>
2020-05-18 14:21:04 -0400

    Updates

--- App.js
+++ App.js
@@ -1,30 +1,62 @@
 import React from 'react';
-import {Image} from 'react-native'
-import { createAppContainer, createStackNavigator } from 'react-navigation';
-import Welcome from '../PlantApp/screens/Welcome'
-import Login from '../PlantApp/screens/Login'
-import Browse from '../PlantApp/screens/Browse'
-import Explore from '../PlantApp/screens/Explore'
-import Settings from '../PlantApp/screens/Settings'
-import Product from '../PlantApp/screens/Product'
+import {StyleSheet, Text, View} from 'react-native';
+import { AppLoading, Asset} from 'expo';
 
-import {theme} from '../PlantApp/constants'
+import Navigation from './navigation';
+import {Block} from './components';
+import * as constants from './constants';
 
-const screens = createStackNavigator({
-  Welcome,
-  Login,
-  Explore,
-  Browse,
-  Product,
-  Settings,
-}, {
-  defaultNavigationOptions: {
-    headerStyle: {}, 
-    headerBlackImage: <Image/>,
-    headerBlackTitle: null,
-    headerLeftContainerStyle: {}, 
-    headerRightContainerStyle: {}, 
+//import all images
+const images = [
+  require('./assets/icons/back.png'),
+  require('./assets/icons/plants.png'),
+  require('./assets/icons/seed.png'),
+  require('./assets/icons/flowers.png'),
+  require('./assets/icons/sprayers.png'),
+  require('./assets/icons/pots.png'),
+  require('./assets/icons/fertilizers.png'),
+  require('./assets/images/plants_1.png'),
+  require('./assets/images/plants_2.png'),
+  require('./assets/images/plants_3.png'),
+  require('./assets/images/explore_1.png'),
+  require('./assets/images/explore_2.png'),
+  require('./assets/images/explore_3.png'),
+  require('./assets/images/explore_4.png'),
+  require('./assets/images/explore_5.png'),
+  require('./assets/images/explore_6.png'),
+  require('./assets/images/avatar.png'),
+];
+
+export default class App extends React.Component{
+  state = {
+    isLoadingComplete: false,
   }
-});
 
-export default createAppContainer(screens);
+  handleResourcesAsync = async () => {
+
+    //Caching images
+    const cacheImages = images.map(img=>{
+      return Asset.fromModule(image).downloadAsync();
+    });
+
+    return Promise.all(cacheImages);
+  }
+
+  render()
+  {
+    if(this.state.isLoadingComplete && !this.props.skipLoadingScreen){
+      return (
+        <AppLoading>
+          startAsync = {this.handleResourcesAsync}
+          onError = {error => console.warn(error)}
+          onFinish={() => this.setState({isLoadingComplete: true})}
+        </AppLoading>
+      )
+    }
+    return (
+      <View style={StyleSheet.container}>
+        <Navigation/>
+      </View>
+    );
+  }
+}
---
+++ navigation/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Image} from 'react-native'
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+import Welcome from '../PlantApp/screens/Welcome'
+// import Login from '../PlantApp/screens/Login'
+// import Browse from '../PlantApp/screens/Browse'
+// import Explore from '../PlantApp/screens/Explore'
+// import Settings from '../PlantApp/screens/Settings'
+// import Product from '../PlantApp/screens/Product'
+
+import {theme} from '../PlantApp/constants'
+
+const screens = createStackNavigator({
+  Welcome,
+  // Login,
+  // Explore,
+  // Browse,
+  // Product,
+  // Settings,
+}, {
+  defaultNavigationOptions: {
+    headerStyle: {}, 
+    headerBlackImage: <Image/>,
+    headerBlackTitle: null,
+    headerLeftContainerStyle: {}, 
+    headerRightContainerStyle: {}, 
+  }
+});
+
+export default createAppContainer(screens);
