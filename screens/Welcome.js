73c170c42fbc2d9a4ad7146568f08d4e4a07131d
PavneetG <pavneetgill2@live.com>
2020-05-18 19:01:08 -0400

    Added Components

---
+++ screens/Welcome.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import {Text, StyleSheet, View} from 'react-native'
+
+import { Button, Block, Text } from '../components'
+
+export default class Welcome extends Component {
+    render() {
+        return(
+            <View>
+                <Text>Welcome</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+
+})
